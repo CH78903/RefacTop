fd988e1f98ae4f2d5dd3885e4643cd323359e353
CH78903 <cris0910lopez@gmail.com>
2026-05-18 16:03:33 -0700

    Complemente views.py para que funcione

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,7 +1,15 @@
-om django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Auto
 
 def home(request):
     busqueda = request.GET.get('buscar')
     if busqueda:
         autos = Auto.objects.filter(modelo__icontains=busqueda)
+
+    else:
+        autos = Auto.objects.all()
+    return render(request, 'catalogo/index.html', {'autos': autos, 'busqueda': busqueda})
+
+def detalle(request, auto_id):
+    auto = get_object_or_404(Auto, pk=auto_id)
+    return render(request, 'catalogo/detalle_auto.html', {'auto': auto})
