81b23d8ef4d9a4dc435bd8b7cfb17282899ec2c0
raffloresx <rafaelfloresh900@gmail.com>
2026-05-18 15:40:29 -0700

    puse las tres tablas de models.py que son marca,vendedor y sucursal

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Marca(models.Model):
+    nombre = models.CharField(max_length=50)
+    pais_origen = models.CharField(max_length=50, blank=True)
+    def __str__(self): return self.nombre
+
+class Vendedor(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=20)
+    def __str__(self): return self.nombre
+
+class Sucursal(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    def __str__(self): return self.nombre
+
