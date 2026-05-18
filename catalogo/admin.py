d075eae7701f60029e8746af345a148659d6fb3e
gonzalezanahy021 <gonzalezanahy021@gmail.com>
2026-05-18 15:52:39 -0700

    meti los registros de las seis columnas en admin.py

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Auto, Marca, Vendedor, Sucursal, Cliente, Venta
 
-# Register your models here.
+admin.site.register(Auto)
+admin.site.register(Marca)
+admin.site.register(Vendedor)
+admin.site.register(Sucursal)
+admin.site.register(Cliente)
+admin.site.register(Venta)
